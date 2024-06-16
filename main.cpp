ab06b76b6ba77d791c1be49f966114d8f738c490
temurazovan <nadezhdadieslast@gmail.com>
2024-06-16 14:15:45 +0400

    found two numbers in the array, the sum of which gives the result

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+int sum(int arr[], int result, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size; j++) {
+            if (result == arr[i] + arr[j]) {
+                std::cout << arr[i] << " " << arr[j];
+                return 0;
+            }
+        }
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int array[] = {7, 7, 11, 15, 2};
+    int size = sizeof(array)/ sizeof(array[0]);
+    int result = 9;
+    sum(array, result, size);
     return 0;
 }
